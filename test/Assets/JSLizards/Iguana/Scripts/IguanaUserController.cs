0b9f41f76fc0fef82a839e64a8557a0effc2139a
xXArkantosXx <36973464+xXArkantosXx@users.noreply.github.com>
2018-03-03 12:50:16 -0500

    Proyecto init
    Inicio del Proyecto en el repositorio

---
+++ test/Assets/JSLizards/Iguana/Scripts/IguanaUserController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class IguanaUserController : MonoBehaviour
+{
+    IguanaCharacter iguanaCharacter;
+
+    void Start()
+    {
+        iguanaCharacter = GetComponent<IguanaCharacter>();
+    }
+
+    //void Update () {	
+    //    if (Input.GetButtonDown ("Fire1")) {
+    //        iguanaCharacter.Attack();
+    //    }
+
+    //    if (Input.GetKeyDown (KeyCode.H)) {
+    //        iguanaCharacter.Hit();
+    //    }
+
+    //    if (Input.GetKeyDown (KeyCode.E)) {
+    //        iguanaCharacter.Eat();
+    //    }
+
+    //    if (Input.GetKeyDown (KeyCode.K)) {
+    //        iguanaCharacter.Death();
+    //    }
+
+    //    if (Input.GetKeyDown (KeyCode.R)) {
+    //        iguanaCharacter.Rebirth();
+    //    }		
+
+
+
+    //}
+
+    bool eat = false;
+    void Update()
+    {
+        if (eat)
+        {
+            Eat();
+        }
+        NoEat();
+    }
+
+    public void Eat()
+    {
+        eat = true;
+        iguanaCharacter.Eat();
+        Debug.Log("la iguana empieza a comer");
+    }
+    public void NoEat()
+    {
+        eat = false;
+        Debug.Log("la iguana deja de comer");
+    }
+
+
+    //private void FixedUpdate()
+    //{
+    //    float h = Input.GetAxis ("Horizontal");
+    //    float v = Input.GetAxis ("Vertical");
+    //    iguanaCharacter.Move (v,h);
+    //}
+}
