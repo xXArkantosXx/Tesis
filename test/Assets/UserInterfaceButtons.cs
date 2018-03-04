2f493fec56aa56479461e9b446a9db643b3b7b5a
xXArkantosXx <36973464+xXArkantosXx@users.noreply.github.com>
2018-03-03 19:45:07 -0500

    ajustes

--- test/Assets/UserInterfaceButtons.cs
+++ test/Assets/UserInterfaceButtons.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.IO;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UserInterfaceButtons : MonoBehaviour
 {
--- test/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ test/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -1,112 +1,101 @@
-/*==============================================================================
-Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
-All Rights Reserved.
-Confidential and Proprietary - Protected under copyright and other laws.
-==============================================================================*/
-
 using UnityEngine;
 
 namespace Vuforia
 {
-    /// <summary>
-    /// A custom handler that implements the ITrackableEventHandler interface.
-    /// </summary>
-    public class DefaultTrackableEventHandler : MonoBehaviour,
-                                                ITrackableEventHandler
-    {
-        #region PRIVATE_MEMBER_VARIABLES
- 
-        private TrackableBehaviour mTrackableBehaviour;
-    
-        #endregion // PRIVATE_MEMBER_VARIABLES
+	/// <summary>
+	/// A custom handler that implements the ITrackableEventHandler interface.
+	/// </summary>
+	public class DefaultTrackableEventHandler : MonoBehaviour,
+	ITrackableEventHandler
+	{
+		#region PRIVATE_MEMBER_VARIABLES
+
+		private TrackableBehaviour mTrackableBehaviour;
+
+		#endregion // PRIVATE_MEMBER_VARIABLES
+
+		#region UNTIY_MONOBEHAVIOUR_METHODS
+
+		void Start()
+		{
+			mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+			if (mTrackableBehaviour)
+			{
+				mTrackableBehaviour.RegisterTrackableEventHandler(this);
+			}
+		}
+
+		#endregion // UNTIY_MONOBEHAVIOUR_METHODS
 
 
 
-        #region UNTIY_MONOBEHAVIOUR_METHODS
-    
-        void Start()
-        {
-            mTrackableBehaviour = GetComponent<TrackableBehaviour>();
-            if (mTrackableBehaviour)
-            {
-                mTrackableBehaviour.RegisterTrackableEventHandler(this);
-            }
-        }
+		#region PUBLIC_METHODS
 
-        #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+		/// <summary>
+		/// Implementation of the ITrackableEventHandler function called when the
+		/// tracking state changes.
+		/// </summary>
+		public void OnTrackableStateChanged(
+			TrackableBehaviour.Status previousStatus,
+			TrackableBehaviour.Status newStatus)
+		{
+			if (newStatus == TrackableBehaviour.Status.DETECTED ||
+				newStatus == TrackableBehaviour.Status.TRACKED ||
+				newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+			{
+				OnTrackingFound();
+			}
+			else
+			{
+				OnTrackingLost();
+			}
+		}
 
-
-
-        #region PUBLIC_METHODS
-
-        /// <summary>
-        /// Implementation of the ITrackableEventHandler function called when the
-        /// tracking state changes.
-        /// </summary>
-        public void OnTrackableStateChanged(
-                                        TrackableBehaviour.Status previousStatus,
-                                        TrackableBehaviour.Status newStatus)
-        {
-            if (newStatus == TrackableBehaviour.Status.DETECTED ||
-                newStatus == TrackableBehaviour.Status.TRACKED ||
-                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
-            {
-                OnTrackingFound();
-            }
-            else
-            {
-                OnTrackingLost();
-            }
-        }
-
-        #endregion // PUBLIC_METHODS
+		#endregion // PUBLIC_METHODS
 
 
 
-        #region PRIVATE_METHODS
+
+		private void OnTrackingFound()
+		{
+			Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+			Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+			// Enable rendering:
+			foreach (Renderer component in rendererComponents)
+			{
+				component.enabled = true;
+			}
+
+			// Enable colliders:
+			foreach (Collider component in colliderComponents)
+			{
+				component.enabled = true;
+			}
+
+			Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+		}
 
 
-        private void OnTrackingFound()
-        {
-            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
-            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+		private void OnTrackingLost()
+		{
+			Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+			Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
 
-            // Enable rendering:
-            foreach (Renderer component in rendererComponents)
-            {
-                component.enabled = true;
-            }
+			// Disable rendering:
+			foreach (Renderer component in rendererComponents)
+			{
+				component.enabled = false;
+			}
 
-            // Enable colliders:
-            foreach (Collider component in colliderComponents)
-            {
-                component.enabled = true;
-            }
+			// Disable colliders:
+			foreach (Collider component in colliderComponents)
+			{
+				component.enabled = false;
+			}
 
-            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
-        }
+			Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+		}
 
-
-        private void OnTrackingLost()
-        {
-            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
-            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
-
-            // Disable rendering:
-            foreach (Renderer component in rendererComponents)
-            {
-                component.enabled = false;
-            }
-
-            // Disable colliders:
-            foreach (Collider component in colliderComponents)
-            {
-                component.enabled = false;
-            }
-
-            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
-        }
-
-        #endregion // PRIVATE_METHODS
-    }
+	}
 }
